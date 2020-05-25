ef24f75e3b0dc05ac657968dd5fc3b44bd25ef8d
Lariane <Lariane@Megaware>
2020-05-25 10:59:28 -0300

    comiti inicial exercicios

---
+++ src/exercicio_modulo2/Exercicio_modulo2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio_modulo2;
+
+/**
+ *
+ * @author Lariane
+ */
+public class Exercicio_modulo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuPrincipalExercicio tela = new MenuPrincipalExercicio();
+        
+        tela.setVisible( true );
+    }
+    
+}
